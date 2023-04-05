0c25f642b5ecdadadb92933e038a7464fed2abee
arunachalamuthu <muthuvel171998@gmail.com>
2023-04-05 07:46:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+const find =()=>{
+  fetch('https://today-server.vercel.app/')
+  .then((res)=>res.json())
+  .then((data)=>{
+    console.log(data);
+  })
+}
 function App() {
   return (
-   
+   <div>
+ <button onClick={find}>
+  click
+  </button> 
     </div>
   );
 }
