9b35e84c69e98eae841f3a4abb9ccfed873aacf5
arunachalamuthu <muthuvel171998@gmail.com>
2023-04-05 07:55:26 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name,setname]=useState([])
   const find =()=>{
   
     fetch('https://today-server.vercel.app/api')
     .then((res)=>{
       console.log(res)
-      return res
+      return res.json()
     })
     .then((data)=>{
       console.log(data);
+      setname(data)
     })
   }
   return (
